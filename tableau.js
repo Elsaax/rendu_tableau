1c7638cd90d41dca2dd34277f194ca29c67c41a8
Elsaax <146921872+Elsaax@users.noreply.github.com>
2023-10-06 12:08:46 +0200

    Add files via upload

---
+++ tableau.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let names = [];
+
+names.push("Vincent");
+names.push("Paul");
+names.push("Arthur");
+
+for (let i = 0; i < names.length; i++){
+    names[i] += " va a la peche";
+}
+console.log(names);
